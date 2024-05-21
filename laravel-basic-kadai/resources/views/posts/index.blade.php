6fd3ba199e67f557f052342ff68b46288f9082b0
habanero-love <5kontir0ea@gmail.com>
2024-05-21 22:49:32 +0900

    「クエリビルダを使ってデータを取得しよう」関連ファイルを追加

--- laravel-basic-kadai/resources/views/posts/index.blade.php
+++ laravel-basic-kadai/resources/views/posts/index.blade.php
@@ -1,14 +1,52 @@
 <!DOCTYPE html>
- <html lang="ja">
- 
- <head>
-     <meta charset="UTF-8">
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>Laravel基礎</title>
- </head>
- 
- <body>
-     <h1>投稿一覧</h1>
- </body>
- 
- </html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>クエリビルダを使ってデータを取得しよう</title>
+<style>
+    body{
+        display:flex;
+        align-items:center;
+        min-height:100vh;
+    }
+    table{
+        border-collapse: collapse;
+        max-width:800px;
+        margin:0 auto;
+    }
+    table,td,th{
+        border:1px solid #EE9487;
+    }
+    td,th{
+        padding:10px;
+    }
+    th{
+        background: #E44461;
+        color:white;
+        text-align:center;
+        font-size:18px;
+    }
+    th:first-child{
+        width:30%;
+    }
+</style>
+</head>
+
+<body>
+    <table>
+        <tr>
+            <th>タイトル</th>
+            <th>本文</th>
+        </tr>
+        @foreach($posts as $post)
+            <tr>
+                <td>{{ $post->title }}</td>
+                <td>{{ $post->content }}</td>
+            </tr>            
+        @endforeach       
+    </table>
+</body>
+
+</html>
